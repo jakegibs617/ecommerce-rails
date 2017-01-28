cde13fca000bde77758ae58c7100f11d7597a5b0
Jacob Giberson <jacobgiberson@Jacobs-iMac.local>
2017-01-28 13:02:28 -0500

    add devise, and fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :movies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root 'movies#index'
 
-resources :movies, only: [:show, :index]
+	root 'movies#index'
+	resources :movies, only: [:show, :index]
+	devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
 end
 
