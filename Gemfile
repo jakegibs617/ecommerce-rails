6941151a432e06ae7fdb303054cffc92eb161dd3
Jacob Giberson <jacobgiberson@Jacobs-iMac.local>
2017-01-28 13:55:07 -0500

    add to cart successful with ajax

--- Gemfile
+++ Gemfile
@@ -32,3 +32,5 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'foundation-rails'
 gem 'devise'
+gem 'redis', '~> 3.0.1'
+gem 'hiredis', '~> 0.6.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 
-	root 'movies#index'
-	resources :movies, only: [:show, :index]
-	devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+# Root page
+  root 'movies#index'
+
+  # Devise user authentication
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
+  # Movies
+  resources :movies, only: [:show, :index]
+
+  # Shopping Cart
+  resource :cart, only: [:show] do
+    put 'add/:movie_id', to: 'carts#add', as: :add_to
+    put 'remove/:movie_id', to: 'carts#remove', as: :remove_from
+  end
 
 end
 
