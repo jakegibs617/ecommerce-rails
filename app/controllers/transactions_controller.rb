da30627d8f0041dab7d3c4944cd931a5164ca612
Jacob Giberson <jacobgiberson@Jacobs-iMac.local>
2017-01-30 11:14:37 -0500

    bug fixes include, payment vault for user, refactored code, fixed seed file and youtube video path

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,22 +1,37 @@
 # require 'pry'
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :check_cart!
+  # before_action :check_cart!
 
   def new
   	gon.client_token = generate_client_token
   end
-  
-  def create
-    @result = Braintree::Transaction.sale(
-              amount: current_user.cart_total_price,
-              payment_method_nonce: params[:payment_method_nonce])
-    if @result.success?
-    	# binding.pry 
-    	# for some reason i cant seem to get this to fail with a declined sandbox credit card
 
+  def create
+    unless current_user.has_payment_info?
+      @result = Braintree::Transaction.sale(
+                  amount: current_user.cart_total_price,
+                  payment_method_nonce: params[:payment_method_nonce],
+                  customer: {
+                    first_name: params[:first_name],
+                    last_name: params[:last_name],
+                    company: params[:company],
+                    email: current_user.email,
+                    phone: params[:phone]
+                  },
+                  options: {
+                    store_in_vault: true
+                  })
+    else
+      @result = Braintree::Transaction.sale(
+                  amount: current_user.cart_total_price,
+                  payment_method_nonce: params[:payment_method_nonce])
+    end
+
+    if @result.success?
+      current_user.update(braintree_customer_id: @result.transaction.customer_details.id) unless current_user.has_payment_info?
       current_user.purchase_cart_movies!
-      redirect_to root_url, notice: "Congraulations! Your transaction has been successful!"
+      redirect_to root_url, notice: "Congraulations! Your transaction was successful!"
     else
       flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
       gon.client_token = generate_client_token
@@ -27,12 +42,17 @@ class TransactionsController < ApplicationController
   private
   
   def generate_client_token
-  	Braintree::ClientToken.generate
+    if current_user.has_payment_info?
+      Braintree::ClientToken.generate(customer_id: current_user.braintree_customer_id)
+    else
+      Braintree::ClientToken.generate
+    end
   end
 
-  def check_cart!
-  	if current_user.get_cart_movies.blank?
-  		redirect_to root_url, alert: "Please add stuff to your cart.. its empty.. or something went wrong"
-  	end
-  end
+#not needed anymore
+  # def check_cart!
+  # 	if current_user.get_cart_movies.blank?
+  # 		redirect_to root_url, alert: "Please add a movie to your cart!"
+  # 	end
+  # end
 end
