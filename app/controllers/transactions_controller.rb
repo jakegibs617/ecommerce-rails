e8aab2cb8a9cdeb653b0e25e43998db7c9fda1c4
Jacob Giberson <jacobgiberson@Jacobs-iMac.local>
2017-01-29 23:22:49 -0500

    braintree sandbox credit cards work

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,3 +1,4 @@
+# require 'pry'
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_cart!
@@ -5,8 +6,26 @@ class TransactionsController < ApplicationController
   def new
   	gon.client_token = generate_client_token
   end
+  
+  def create
+    @result = Braintree::Transaction.sale(
+              amount: current_user.cart_total_price,
+              payment_method_nonce: params[:payment_method_nonce])
+    if @result.success?
+    	# binding.pry 
+    	# for some reason i cant seem to get this to fail with a declined sandbox credit card
 
+      current_user.purchase_cart_movies!
+      redirect_to root_url, notice: "Congraulations! Your transaction has been successfully!"
+    else
+      flash[:alert] = "Something went wrong while processing your transaction. Please try again!"
+      gon.client_token = generate_client_token
+      render :new
+    end
+  end
+  
   private
+  
   def generate_client_token
   	Braintree::ClientToken.generate
   end
